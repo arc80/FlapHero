aeebdf752806a944f64e7b60b7cad2e525932ca7
Jeff Preshing <filter-github@preshing.com>
2020-09-16 10:05:23 -0400

    Fix puff draw order

--- src/flapGame/flapGame/Puffs.cpp
+++ src/flapGame/flapGame/Puffs.cpp
@@ -12,7 +12,7 @@ bool Puffs::update(float dt) {
 
 void Puffs::addInstances(Array<PuffShader::InstanceData>& instances) const {
     Random r{this->seed};
-    constexpr u32 numPuffs = 8;
+    constexpr u32 numPuffs = 10;
     float t = min(1.f, this->time + DrawContext::instance()->fracTime * Rate);
     float offset = 1.f - powf(1.f - t, 1.5f);
     Float3 basePos = this->pos + Float3{0.3f, 0, 1.f} * (offset * 0.7f);
@@ -21,16 +21,18 @@ void Puffs::addInstances(Array<PuffShader::InstanceData>& instances) const {
     alpha *= min(1.f, t * 2.5f);
     Float3 axis = Float3{0.2f, -0.4f, 1.f}.normalized();
     float scale = mix(0.3f, 1.f, 1.f - powf(1.f - t, 3.f));
+    float side = 1.f;
     for (u32 i = 0; i < numPuffs; i++) {
         float rotFactor = (s32(r.next32() & 2) - 1) * (r.nextFloat() + 1.f) * 0.6f;
         PuffShader::InstanceData& inst = instances.append();
-        Float3 pos = basePos + Quaternion::fromAxisAngle(axis, i * 2.f * Pi / numPuffs) *
+        Float3 pos = basePos + Quaternion::fromAxisAngle(axis, Pi * ((i + 0.5f) * side / numPuffs + 0.5f)) *
                                    Float3{mix(0.4f, 1.2f, distance), 0, 0};
         inst.modelToWorld = Float4x4::makeTranslation(pos) *
                             Float4x4::makeRotation({1, 0, 0}, Pi / 2.f) *
                             Float4x4::makeRotation({0, 0, 1}, (t + 15.f) * rotFactor) *
                             Float4x4::makeScale(0.6f * scale);
-        inst.colorAlpha = {this->time, alpha * 0.8f};
+        inst.colorAlpha = {this->time, alpha * 0.7f};
+        side *= -1.f;
     }
 }
 
